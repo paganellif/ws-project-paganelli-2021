369c7e25814fda9e906fb1027aaadf53c7864a29
Filippo Paganelli <filippo1paganelli@gmail.com>
2021-07-03 15:37:38 +0200

    add requirements; test rdflib

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='ws-project-paganelli-2021',
+    version='0.0.1',
+    packages=[''],
+    url='',
+    license='MIT',
+    author='filo',
+    author_email='filippo1paganelli@gmail.com',
+    description='ws-project-paganelli-2021'
+)
