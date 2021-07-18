ff2a1900bd022c033cc379935a4bce29f8fda8f4
Filippo Paganelli <filippo1paganelli@gmail.com>
2021-07-18 19:48:33 +0200

    added new examples wsn-mas

---
+++ assignment_1/wsn-ffd.py
@@ -0,0 +1,7 @@
+from rdflib import Namespace
+
+if __name__ == '__main__':
+    # https://www.w3.org/2005/Incubator/ssn/ssnx/ssn
+    SSN: Namespace = Namespace('http://purl.oclc.org/NET/ssnx/ssn/')
+
+    
