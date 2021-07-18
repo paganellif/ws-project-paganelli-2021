029479585113830a47f34aab0047983775896cd9
Filippo Paganelli <filippo1paganelli@gmail.com>
2021-07-18 19:48:58 +0200

    added new examples wsn-mas

--- assignment_1/wsn-ffd.py
+++ assignment_1/wsn-ffd.py
@@ -1,7 +1,32 @@
-from rdflib import Namespace
+from rdflib import Namespace, Graph, URIRef, RDF
+
+
+def print_graph(graph: Graph):
+    for subj, pred, obj in graph:
+        # check if there is at least one triple in the Graph
+        if (subj, pred, obj) not in graph:
+            raise Exception("No triple in the graph!")
+        else:
+            print("Subject: "+subj+" Predicate: "+pred+" Object: "+obj)
+
 
 if __name__ == '__main__':
-    # https://www.w3.org/2005/Incubator/ssn/ssnx/ssn
-    SSN: Namespace = Namespace('http://purl.oclc.org/NET/ssnx/ssn/')
 
-    
+    # Create Forest Fire Detection Graph
+    ffd_graph: Graph = Graph()
+
+    SOSA: Namespace = Namespace(' http://www.w3.org/ns/sosa/')
+
+    ffd_graph.namespace_manager.bind('sosa', SOSA)
+
+    thermistor_a = URIRef("http://example.org/thermistor_a")
+    ffd_graph.add((thermistor_a, RDF.type, SOSA.Sensor))
+
+    temperature = URIRef("http://example.org/temperature")
+    ffd_graph.add((temperature, RDF.type, SOSA.ObservableProperty))
+
+    ffd_graph.add((thermistor_a, SOSA.observes, temperature))
+    ffd_graph.add((temperature, SOSA.isObservedBy, thermistor_a))
+
+    # Print graph
+    print_graph(ffd_graph)
--- main.py
+++ main.py
@@ -2,31 +2,12 @@ import rdflib
 from rdflib import URIRef, RDF, FOAF, Literal, XSD
 
 if __name__ == '__main__':
-    '''
-    # create a Graph
-    g = rdflib.Graph()
 
-    # parse in an RDF file hosted on the Internet
-    result = g.parse("http://www.w3.org/People/Berners-Lee/card")
-
-    # loop through each triple in the graph (subj, pred, obj)
-    for subj, pred, obj in g:
-        # check if there is at least one triple in the Graph
-        if (subj, pred, obj) not in g:
-            raise Exception("It better be!")
-
-    # print the number of "triples" in the Graph
-    print("graph has {} statements.".format(len(g)))
-    # prints graph has 86 statements.
-
-    # print out the entire Graph in the RDF Turtle format
-    print(g.serialize(format="turtle").decode("utf-8"))
-    '''
     # create a Graph
     g = rdflib.Graph()
 
     # Create an RDF URI node to use as the subject for multiple triples
-    donna = URIRef("http://example.org/donna")
+    donna = URIRef("http://example.orzg/donna")
 
     # Add triples using store's add() method.
     g.add((donna, RDF.type, FOAF.Person))
