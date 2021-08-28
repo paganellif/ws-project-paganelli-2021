64bec571ad97307fc752c643803f33e489f54bc8
FilippoPaganelli <filippo1paganelli@gmail.com>
2021-08-28 11:07:26 +0200

    fix rdf:resources imported rdf

--- wsn-ffd-owl.py
+++ wsn-ffd-owl.py
@@ -1,5 +1,5 @@
 from rdflib import Graph
-from rdflib.term import URIRef, Literal
+from rdflib.term import URIRef, Literal, BNode
 from rdflib.namespace import Namespace, RDF
 import os
 
@@ -12,21 +12,18 @@ if __name__ == '__main__':
     base_ontology: str = 'https://wsn-ffd-owl.org/'
 
     ##############################
-    # Binding Namespaces
+    # Binding/Import Namespaces
     ##############################
     OWL: Namespace = Namespace('http://www.w3.org/2002/07/owl#')
     wsd_ffd_owl.bind('owl', OWL)
 
     # owl:imports 'https://wsn-ffd-rdf.org/' --> in realtà metto il link alla ontologia sul repo gitlab
-    # che magari la scarica davvero
-    # https://gitlab.com/paganelli.f/ws-project-paganelli-2021/-/raw/develop/wsd-ffd-pretty-xml.rdf
-    rdf_import_ontology = 'https://gitlab.com/paganelli.f/ws-project-paganelli-2021/-/raw/develop/wsd-ffd-pretty-xml.rdf'
-
+    my_ontology_uri = 'https://gitlab.com/paganelli.f/ws-project-paganelli-2021/-/raw/develop/wsd-ffd-pretty-xml.rdf'
 
     ontology = URIRef(value='wsn-mas-ontology', base=base_ontology)
     wsd_ffd_owl.add((ontology, RDF.type, OWL.Ontology))
     wsd_ffd_owl.add((ontology, OWL.versionInfo, Literal('1.0')))
-    wsd_ffd_owl.add((ontology, OWL.imports, Literal(rdf_import_ontology)))
+    wsd_ffd_owl.add((ontology, OWL.imports, URIRef(value=my_ontology_uri)))
 
 
     # fare esempi di proprietà inverse di alcune proprietà definite nel precedente RDF
